5e81815d7912c5d07654f9c631d7bc40dacd4b5d
Den <pugachovdenys@gmail.com>
2022-01-10 20:15:03 +0200

    feat: render slected move = stepNumber

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)//if “go back in time” => delete “future” history that would now become incorrect
     const current = history[history.length - 1]
     const squares = { ...current.squares }
 
@@ -67,11 +67,12 @@ class Game extends React.Component {
 
     squares[i] = this.state.xIsNext ? "X" : "O"
 
-    // concatenate new history entries onto history.
+    // add new history squares:[] onto history.
     this.setState({
       history: history.concat([{
         squares
       }]),
+      stepNumber: history.length,// ensures we don’t get stuck showing the same move after a new one has been made
       xIsNext: !this.state.xIsNext
     })
   }
@@ -79,13 +80,13 @@ class Game extends React.Component {
   jumpTo(step) {
     this.setState({
       stepNumber: step,
-      xIsNext: (step % 2) === 0,
+      xIsNext: (step % 2) === 0, //:bool
     })
   }
 
   render() {
     const history = this.state.history
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber] //rendering currently selected move => stepNumber
     const winner = calculateWinner(current.squares)
 
     const moves = history.map((step, move) => {
